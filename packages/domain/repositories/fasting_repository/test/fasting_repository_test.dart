158c21ed34036384c573596cc2e56405c88e6eae
Sjoerd Lemkens <97674344+sjoerdlmkns@users.noreply.github.com>
2025-12-01 19:58:18 +0100

    test: first fasting repo test

---
+++ packages/domain/repositories/fasting_repository/test/fasting_repository_test.dart
@@ -0,0 +1,10 @@
+import 'package:fasting_repository/fasting_repository.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('FastingRepository', () {
+    test('class exists and can be referenced', () {
+      expect(FastingRepository, isNotNull);
+    });
+  });
+}
