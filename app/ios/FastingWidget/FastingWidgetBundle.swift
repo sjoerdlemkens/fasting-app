ad47d922b2fc6b5e27ef0e4e0f03fb671ef68b45
Sjoerd Lemkens <97674344+sjoerdlemkens@users.noreply.github.com>
2026-06-02 15:44:20 +0200

    wip: added basic home widget

---
+++ app/ios/FastingWidget/FastingWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  FastingWidgetBundle.swift
+//  FastingWidget
+//
+//  Created by Sjoerd 2 on 02/06/2026.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct FastingWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        FastingWidget()
+    }
+}
