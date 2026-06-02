eb43a2cbd2c6df38c2504f3c9f7df59d8e83f2c7
Sjoerd Lemkens <97674344+sjoerdlemkens@users.noreply.github.com>
2026-06-02 13:08:59 +0200

    test: delete tests for now

--- app/test/widget_test.dart
+++
@@ -1,8 +0,0 @@
-// import 'package:fasting_app/app/app.dart';
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  testWidgets('Load App', (WidgetTester tester) async {
-    // await tester.pumpWidget(const App());
-  });
-}
